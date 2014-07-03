86cd117c2594653ba94463057e4435d1147babcc
King-Fisher <silverykingfisher@outlook.com>
2014-07-03 05:44:15 +0200

    It's now possible to zoom in the charts.

--- src/main/java/com/kingfisher/servermonitor/DataViewerServer.java
+++ src/main/java/com/kingfisher/servermonitor/DataViewerServer.java
@@ -96,6 +96,10 @@ public final class DataViewerServer implements Closeable {
 			}
 		}
 
+		private String getP(String id) {
+			return "        <p id=\"" + id + "\" style=\"border: solid 1px #000000\"></p>\n";
+		}
+
 		@Override
 		public void run() {
 			try {
@@ -112,39 +116,14 @@ public final class DataViewerServer implements Closeable {
 								StringBuilder function = new StringBuilder();
 								StringBuilder body = new StringBuilder();
 								try {
-									if (_serverMonitor.getConfiguration().getMonitorBlocks()) {
-										function.append("            new google.visualization.LineChart(document.getElementById('blocks_broken')).draw(new google.visualization.arrayToDataTable([['Date', 'Broken']");
-										ResultSet result = _serverMonitor.getSQLBridge().executeQuery("SELECT * FROM server_monitor_blocks");
-										while (result.next()) {
-											function.append(", ['").append(result.getString("date")).append("', ").append(result.getInt("broken")).append(']');
-										}
-										function.append("]), {'title':'Broken blocks', 'height':440, 'colors':['red'], 'explorer':{axis:'horizontal'}});\n");
-										body.append("        <p id=\"blocks_broken\"></p>\n");
-										function.append("            new google.visualization.LineChart(document.getElementById('blocks_placed')).draw(new google.visualization.arrayToDataTable([['Date', 'Placed']");
-										result.beforeFirst();
-										while (result.next()) {
-											function.append(", ['").append(result.getString("date")).append("', ").append(result.getInt("placed")).append(']');
-										}
-										function.append("]), {'title':'Placed blocks', 'height':440, 'colors':['green'], 'explorer':{axis:'horizontal'}});\n");
-										body.append("        <p id=\"blocks_placed\"></p>\n");
-									}
-									if (_serverMonitor.getConfiguration().getMonitorChunks()) {
-										function.append("            new google.visualization.LineChart(document.getElementById('chunks')).draw(new google.visualization.arrayToDataTable([['Date', 'Mean', 'Minimum', 'Maximum']");
-										ResultSet result = _serverMonitor.getSQLBridge().executeQuery("SELECT * FROM server_monitor_chunks");
+									if (_serverMonitor.getConfiguration().getMonitorPlayers()) {
+										function.append("            new google.visualization.LineChart(document.getElementById('players')).draw(new google.visualization.arrayToDataTable([['Date', 'Mean', 'Minimum', 'Maximum']");
+										ResultSet result = _serverMonitor.getSQLBridge().executeQuery("SELECT * FROM server_monitor_players");
 										while (result.next()) {
 											function.append(", ['").append(result.getString("date")).append("', ").append(result.getFloat("mean")).append(", ").append(result.getInt("min")).append(", ").append(result.getInt("max")).append(']');
 										}
-										function.append("]), {'title':'Loaded chunks', 'height':440, 'colors':['green', 'blue', 'red'], 'explorer':{axis:'horizontal'}});\n");
-										body.append("        <p id=\"chunks\"></p>\n");
-									}
-									if (_serverMonitor.getConfiguration().getMonitorClassLoading()) {
-										function.append("            new google.visualization.LineChart(document.getElementById('class_loading')).draw(new google.visualization.arrayToDataTable([['Date', 'Mean', 'Minimum', 'Maximum']");
-										ResultSet result = _serverMonitor.getSQLBridge().executeQuery("SELECT * FROM server_monitor_class_loading");
-										while (result.next()) {
-											function.append(", ['").append(result.getString("date")).append("', ").append(result.getFloat("loaded_mean")).append(", ").append(result.getInt("loaded_min")).append(", ").append(result.getInt("loaded_max")).append(']');
-										}
-										function.append("]), {'title':'Loaded classes', 'height':440, 'colors':['green', 'blue', 'red'], 'explorer':{axis:'horizontal'}});\n");
-										body.append("        <p id=\"class_loading\"></p>\n");
+										function.append("]), {'title':'Players monitoring (player count)', 'height':440, 'colors':['green', 'blue', 'red'], 'explorer':{keepInBounds:true, maxZoomIn:0.125}, 'vAxis':{minValue:0}});\n");
+										body.append(getP("players"));
 									}
 									if (_serverMonitor.getConfiguration().getMonitorEntities()) {
 										function.append("            new google.visualization.LineChart(document.getElementById('entities')).draw(new google.visualization.arrayToDataTable([['Date', 'Mean', 'Minimum', 'Maximum']");
@@ -152,78 +131,80 @@ public final class DataViewerServer implements Closeable {
 										while (result.next()) {
 											function.append(", ['").append(result.getString("date")).append("', ").append(result.getFloat("mean")).append(", ").append(result.getInt("min")).append(", ").append(result.getInt("max")).append(']');
 										}
-										function.append("]), {'title':'Entities', 'height':440, 'colors':['green', 'blue', 'red'], 'explorer':{axis:'horizontal'}});\n");
-										body.append("        <p id=\"entities\"></p>\n");
+										function.append("]), {'title':'Entities monitoring (entity count)', 'height':440, 'colors':['green', 'blue', 'red'], 'explorer':{keepInBounds:true, maxZoomIn:0.125}, 'vAxis':{minValue:0}});\n");
+										body.append(getP("entities"));
 									}
-									if (_serverMonitor.getConfiguration().getMonitorExperience()) {
-										//
-									}
-									if (_serverMonitor.getConfiguration().getMonitorGarbageCollector()) {
-										function.append("            new google.visualization.LineChart(document.getElementById('garbage_collector')).draw(new google.visualization.arrayToDataTable([['Date', 'Collected objects']");
-										ResultSet result = _serverMonitor.getSQLBridge().executeQuery("SELECT * FROM server_monitor_garbage_collector");
+									if (_serverMonitor.getConfiguration().getMonitorBlocks()) {
+										function.append("            new google.visualization.LineChart(document.getElementById('blocks')).draw(new google.visualization.arrayToDataTable([['Date', 'Broken blocks', 'Placed blocks']");
+										ResultSet result = _serverMonitor.getSQLBridge().executeQuery("SELECT * FROM server_monitor_blocks");
 										while (result.next()) {
-											function.append(", ['").append(result.getString("date")).append("', ").append(result.getLong("collected")).append(']');
+											function.append(", ['").append(result.getString("date")).append("', ").append(result.getInt("broken")).append(", ").append(result.getInt("placed")).append(']');
 										}
-										function.append("]), {'title':'Garbage collector', 'height':440, 'colors':['blue'], 'explorer':{axis:'horizontal'}});\n");
-										body.append("        <p id=\"garbage_collector\"></p>\n");
+										function.append("]), {'title':'Blocks monitoring', 'height':440, 'colors':['red', 'green'], 'explorer':{keepInBounds:true, maxZoomIn:0.125}, 'vAxis':{minValue:0}});\n");
+										body.append(getP("blocks"));
 									}
-									if (_serverMonitor.getConfiguration().getMonitorItems()) {
-										//
+									if (_serverMonitor.getConfiguration().getMonitorChunks()) {
+										function.append("            new google.visualization.LineChart(document.getElementById('chunks')).draw(new google.visualization.arrayToDataTable([['Date', 'Mean', 'Minimum', 'Maximum']");
+										ResultSet result = _serverMonitor.getSQLBridge().executeQuery("SELECT * FROM server_monitor_chunks");
+										while (result.next()) {
+											function.append(", ['").append(result.getString("date")).append("', ").append(result.getFloat("mean")).append(", ").append(result.getInt("min")).append(", ").append(result.getInt("max")).append(']');
+										}
+										function.append("]), {'title':'Chunks monitoring (loaded chunks)', 'height':440, 'colors':['green', 'blue', 'red'], 'explorer':{keepInBounds:true, maxZoomIn:0.125}, 'vAxis':{minValue:0}});\n");
+										body.append(getP("chunks"));
+									}
+									if (_serverMonitor.getConfiguration().getMonitorRedstone()) {
+										function.append("            new google.visualization.LineChart(document.getElementById('redstone')).draw(new google.visualization.arrayToDataTable([['Date', 'Redstone use', 'Pistons use']");
+										ResultSet result = _serverMonitor.getSQLBridge().executeQuery("SELECT * FROM server_monitor_redstone");
+										while (result.next()) {
+											function.append(", ['").append(result.getString("date")).append("', ").append(result.getInt("redstone")).append(", ").append(result.getInt("pistons")).append(']');
+										}
+										function.append("]), {'title':'Redstone monitoring', 'height':440, 'colors':['red', 'green'], 'explorer':{keepInBounds:true, maxZoomIn:0.125}, 'vAxis':{minValue:0}});\n");
+										body.append(getP("redstone"));
 									}
 									if (_serverMonitor.getConfiguration().getMonitorLivingEntities()) {
 										//
 									}
-									if (_serverMonitor.getConfiguration().getMonitorMemory()) {
-										function.append("            new google.visualization.LineChart(document.getElementById('memory')).draw(new google.visualization.arrayToDataTable([['Date', 'Mean', 'Minimum', 'Maximum']");
-										ResultSet result = _serverMonitor.getSQLBridge().executeQuery("SELECT * FROM server_monitor_memory");
-										while (result.next()) {
-											function.append(", ['").append(result.getString("date")).append("', ").append(result.getFloat("total_mean")).append(", ").append(result.getInt("total_min")).append(", ").append(result.getInt("total_max")).append(']');
-										}
-										function.append("]), {'title':'Used memory', 'height':440, 'colors':['green', 'blue', 'red'], 'explorer':{axis:'horizontal'}});\n");
-										body.append("        <p id=\"memory\"></p>\n");
+									if (_serverMonitor.getConfiguration().getMonitorItems()) {
+										//
 									}
-									if (_serverMonitor.getConfiguration().getMonitorOperatingSystem()) {
-										function.append("            new google.visualization.LineChart(document.getElementById('operating_system')).draw(new google.visualization.arrayToDataTable([['Date', 'Mean', 'Minimum', 'Maximum']");
-										ResultSet result = _serverMonitor.getSQLBridge().executeQuery("SELECT * FROM server_monitor_operating_system");
-										while (result.next()) {
-											function.append(", ['").append(result.getString("date")).append("', ").append(result.getFloat("load_mean")).append(", ").append(result.getInt("load_min")).append(", ").append(result.getInt("load_max")).append(']');
-										}
-										function.append("]), {'title':'System load average', 'height':440, 'colors':['green', 'blue', 'red'], 'explorer':{axis:'horizontal'}});\n");
-										body.append("        <p id=\"operating_system\"></p>\n");
-									}
-									if (_serverMonitor.getConfiguration().getMonitorPlayers()) {
-										function.append("            new google.visualization.LineChart(document.getElementById('players')).draw(new google.visualization.arrayToDataTable([['Date', 'Mean', 'Minimum', 'Maximum']");
-										ResultSet result = _serverMonitor.getSQLBridge().executeQuery("SELECT * FROM server_monitor_players");
-										while (result.next()) {
-											function.append(", ['").append(result.getString("date")).append("', ").append(result.getFloat("mean")).append(", ").append(result.getInt("min")).append(", ").append(result.getInt("max")).append(']');
-										}
-										function.append("]), {'title':'Players', 'height':440, 'colors':['green', 'blue', 'red'], 'explorer':{axis:'horizontal'}});\n");
-										body.append("        <p id=\"players\"></p>\n");
-									}
-									if (_serverMonitor.getConfiguration().getMonitorRedstone()) {
-										function.append("            new google.visualization.LineChart(document.getElementById('redstone')).draw(new google.visualization.arrayToDataTable([['Date', 'Redstone use']");
-										ResultSet result = _serverMonitor.getSQLBridge().executeQuery("SELECT * FROM server_monitor_redstone");
-										while (result.next()) {
-											function.append(", ['").append(result.getString("date")).append("', ").append(result.getInt("redstone")).append(']');
-										}
-										function.append("]), {'title':'Redstone use', 'height':440, 'colors':['red'], 'explorer':{axis:'horizontal'}});\n");
-										body.append("        <p id=\"redstone\"></p>\n");
-										function.append("            new google.visualization.LineChart(document.getElementById('pistons')).draw(new google.visualization.arrayToDataTable([['Date', 'Pistons use']");
-										result.beforeFirst();
-										while (result.next()) {
-											function.append(", ['").append(result.getString("date")).append("', ").append(result.getInt("pistons")).append(']');
-										}
-										function.append("]), {'title':'Pistons use', 'height':440, 'colors':['red'], 'explorer':{axis:'horizontal'}});\n");
-										body.append("        <p id=\"pistons\"></p>\n");
+									if (_serverMonitor.getConfiguration().getMonitorExperience()) {
+										//
 									}
 									if (_serverMonitor.getConfiguration().getMonitorTPS()) {
 										function.append("            new google.visualization.LineChart(document.getElementById('tps')).draw(new google.visualization.arrayToDataTable([['Date', 'Mean', 'Minimum', 'Maximum']");
 										ResultSet result = _serverMonitor.getSQLBridge().executeQuery("SELECT * FROM server_monitor_tps");
 										while (result.next()) {
-											function.append(", ['").append(result.getString("date")).append("', ").append(result.getFloat("mean")).append(", ").append(result.getInt("min")).append(", ").append(result.getInt("max")).append(']');
+											function.append(", ['").append(result.getString("date")).append("', ").append(result.getFloat("mean")).append(", ").append(result.getFloat("min")).append(", ").append(result.getFloat("max")).append(']');
 										}
-										function.append("]), {'title':'Ticks per second', 'height':440, 'colors':['green', 'blue', 'red'], 'explorer':{axis:'horizontal'}});\n");
-										body.append("        <p id=\"tps\"></p>\n");
+										function.append("]), {'title':'TPS monitoring (Ticks per second)', 'height':440, 'colors':['green', 'blue', 'red'], 'explorer':{keepInBounds:true, maxZoomIn:0.125}, 'vAxis':{minValue:0}});\n");
+										body.append(getP("tps"));
+									}
+									if (_serverMonitor.getConfiguration().getMonitorMemory()) {
+										function.append("            new google.visualization.LineChart(document.getElementById('memory')).draw(new google.visualization.arrayToDataTable([['Date', 'Mean', 'Minimum', 'Maximum']");
+										ResultSet result = _serverMonitor.getSQLBridge().executeQuery("SELECT * FROM server_monitor_memory");
+										while (result.next()) {
+											function.append(", ['").append(result.getString("date")).append("', ").append(result.getFloat("total_mean")).append(", ").append(result.getFloat("total_min")).append(", ").append(result.getFloat("total_max")).append(']');
+										}
+										function.append("]), {'title':'Memory monitoring (total used memory)', 'height':440, 'colors':['green', 'blue', 'red'], 'explorer':{keepInBounds:true, maxZoomIn:0.125}, 'vAxis':{minValue:0}});\n");
+										body.append(getP("memory"));
+									}
+									if (_serverMonitor.getConfiguration().getMonitorGarbageCollector()) {
+										function.append("            new google.visualization.LineChart(document.getElementById('garbage_collector')).draw(new google.visualization.arrayToDataTable([['Date', 'Collection count']");
+										ResultSet result = _serverMonitor.getSQLBridge().executeQuery("SELECT * FROM server_monitor_garbage_collector");
+										while (result.next()) {
+											function.append(", ['").append(result.getString("date")).append("', ").append(result.getLong("collected")).append(']');
+										}
+										function.append("]), {'title':'Garbage collector monitoring', 'height':440, 'colors':['blue'], 'explorer':{keepInBounds:true, maxZoomIn:0.125}, 'vAxis':{minValue:0}});\n");
+										body.append(getP("garbage_collector"));
+									}
+									if (_serverMonitor.getConfiguration().getMonitorClassLoading()) {
+										function.append("            new google.visualization.LineChart(document.getElementById('class_loading')).draw(new google.visualization.arrayToDataTable([['Date', 'Mean', 'Minimum', 'Maximum']");
+										ResultSet result = _serverMonitor.getSQLBridge().executeQuery("SELECT * FROM server_monitor_class_loading");
+										while (result.next()) {
+											function.append(", ['").append(result.getString("date")).append("', ").append(result.getFloat("loaded_mean")).append(", ").append(result.getInt("loaded_min")).append(", ").append(result.getInt("loaded_max")).append(']');
+										}
+										function.append("]), {'title':'Class loading monitoring (loaded classes)', 'height':440, 'colors':['green', 'blue', 'red'], 'explorer':{keepInBounds:true, maxZoomIn:0.125}, 'vAxis':{minValue:0}});\n");
+										body.append(getP("class_loading"));
 									}
 									if (_serverMonitor.getConfiguration().getMonitorThreads()) {
 										function.append("            new google.visualization.LineChart(document.getElementById('threads')).draw(new google.visualization.arrayToDataTable([['Date', 'Mean', 'Minimum', 'Maximum']");
@@ -231,8 +212,17 @@ public final class DataViewerServer implements Closeable {
 										while (result.next()) {
 											function.append(", ['").append(result.getString("date")).append("', ").append(result.getFloat("all_mean")).append(", ").append(result.getInt("all_min")).append(", ").append(result.getInt("all_max")).append(']');
 										}
-										function.append("]), {'title':'Threads', 'height':440, 'colors':['green', 'blue', 'red'], 'explorer':{axis:'horizontal'}});\n");
-										body.append("        <p id=\"threads\"></p>\n");
+										function.append("]), {'title':'Threads monitoring (thread count)', 'height':440, 'colors':['green', 'blue', 'red'], 'explorer':{keepInBounds:true, maxZoomIn:0.125}, 'vAxis':{minValue:0}});\n");
+										body.append(getP("threads"));
+									}
+									if (_serverMonitor.getConfiguration().getMonitorOperatingSystem()) {
+										function.append("            new google.visualization.LineChart(document.getElementById('operating_system')).draw(new google.visualization.arrayToDataTable([['Date', 'Mean', 'Minimum', 'Maximum']");
+										ResultSet result = _serverMonitor.getSQLBridge().executeQuery("SELECT * FROM server_monitor_operating_system");
+										while (result.next()) {
+											function.append(", ['").append(result.getString("date")).append("', ").append(result.getFloat("load_mean")).append(", ").append(result.getFloat("load_min")).append(", ").append(result.getFloat("load_max")).append(']');
+										}
+										function.append("]), {'title':'Operating system monitoring (system load average)', 'height':440, 'colors':['green', 'blue', 'red'], 'explorer':{keepInBounds:true, maxZoomIn:0.125}, 'vAxis':{minValue:0}});\n");
+										body.append(getP("operating_system"));
 									}
 								} catch (SQLException ex) {
 									_serverMonitor.runtimeException("An unexpected error related to the database.", ex);
